d68fb84e11b17ade13e0600207fafdccdba7200b
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-06-28 22:01:55 +0200

    fix! Refactor dtools
    This change requires a new order of calling the API.
    Now it must be called in this sequence:
    1. get_executable_at_cursor
    2. start_debug_server
    
    This is due to the module caching the metadata to avoid parsing the
    project database more than once

--- lua/dtools.lua
+++ lua/dtools.lua
@@ -6,6 +6,9 @@ local criterion = require("dtools.criterion")
 ---@field additional_args (fun(buf: integer): string[])?
 
 local M = {
+  test_exe = "",
+  test_suite = "",
+  test_name = "",
   ---@type DtoolsOptions
   options = {},
 }
@@ -14,6 +17,22 @@ local M = {
 ---Returns the connection string to the debug server (e.g. localhost:1234) or nil and an optional error message
 ---@return string | nil, string | nil
 function M.start_debug_server()
+  local pid = criterion.start_debug_server(M.test_exe, M.test_suite, M.test_name)
+  if pid <= 0 then
+    return nil, "Failed to start debug server"
+  end
+
+  print("Starting debug server")
+  return "localhost:1234", nil
+end
+
+---Get the test executable that implements the test closest to the cursor
+---@return string | nil, string | nil
+function M.get_executable_at_cursor()
+  M.test_exe = ""
+  M.test_suite = ""
+  M.test_name = ""
+
   local bufnr = vim.api.nvim_get_current_buf()
   local win = vim.api.nvim_get_current_win() -- Get the current active window
   local cursor_pos = vim.api.nvim_win_get_cursor(win) -- Get the cursor position in the window
@@ -25,8 +44,9 @@ function M.start_debug_server()
 
   local builddir = M.options.builddir and M.options.builddir(bufnr) or "build"
   local test_exe = util.get_test_exe_from_buffer(bufnr, builddir)
+  print("Debug exe: " .. vim.inspect(test_exe))
 
-  if test_exe == "" then
+  if test_exe == nil then
     return nil, "No test executable was found in " .. builddir
   end
 
@@ -35,21 +55,12 @@ function M.start_debug_server()
     return nil, "Failed to parse test suite and name"
   end
 
-  local pid = criterion.start_debug_server(test_exe, test.test_suite, test.test_name)
-  if pid <= 0 then
-    return nil, "Failed to start debug server"
-  end
+  M.test_exe = test_exe
+  M.test_name = test.test_name
+  M.test_suite = test.test_suite
+  print("dtools: " .. vim.inspect(M))
 
-  return "localhost:1234", nil
-end
-
----Get the test executable that implements the test closest to the cursor
----@return string | nil
-function M.get_executable_at_cursor()
-  local current_buffer = vim.api.nvim_get_current_buf()
-  local builddir = M.options.builddir and M.options.builddir(current_buffer) or "build"
-  local test_exe = util.get_test_exe_from_buffer(current_buffer, builddir)
-  return test_exe
+  return M.test_exe
 end
 
 --- Plugin options
